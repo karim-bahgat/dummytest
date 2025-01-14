8c5266b7e6a99441d0c7d7ddf50ae5c88bb4c201
karim-bahgat <eng.karimbahgat@gmail.com>
2025-01-14 12:56:08 +0200

    5

--- dummytest.py
+++ dummytest.py
@@ -1,2 +1,2 @@
-print ("hello its me i was wondering if after all this year you would like to meet to go over")
+print ("hello its me i was wondering if after all this year you would like to meet")
 print ("hello")
